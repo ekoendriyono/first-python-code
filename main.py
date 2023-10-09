b84c95640b05979e1adceb1f3285ce663de2745d
Lenovo <eendriyono@gmail.con>
2023-10-09 13:01:58 +0700

    fisrt

---
+++ main.py
@@ -0,0 +1,7 @@
+"""
+ini adalah demo project pertama dengan python
+"""
+print ( "Hello Eko Kamu Bisa")
+print ( "Hello Eko Kamu Bisa")
+print ( "Hello Eko Kamu Bisa")
+print ( "Hello Eko Kamu Bisa")
